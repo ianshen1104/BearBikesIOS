b42b5152c8444b08e7a6dc16034bdf15a95c1b48
Ian Shen <ian891104@berkeley.edu>
2020-04-03 15:08:46 +0800

    First commit

---
+++ Bear BikesTests/Bear_BikesTests.swift
@@ -0,0 +1,34 @@
+//
+//  Bear_BikesTests.swift
+//  Bear BikesTests
+//
+//  Created by Ian Shen on 4/3/20.
+//  Copyright © 2020 Bear Bikes Team. All rights reserved.
+//
+
+import XCTest
+@testable import Bear_Bikes
+
+class Bear_BikesTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
