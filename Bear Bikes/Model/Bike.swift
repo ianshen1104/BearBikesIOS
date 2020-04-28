0a9e7b484761d4d55acf537ce618cb65a83d8077
Ian Shen <ian891104@berkeley.edu>
2020-04-28 09:01:29 +0800

    Added map on storyboard

---
+++ Bear Bikes/Model/Bike.swift
@@ -0,0 +1,19 @@
+//
+//  Bike.swift
+//  Bear Bikes
+//
+//  Created by Ian Shen on 4/28/20.
+//  Copyright © 2020 Bear Bikes Team. All rights reserved.
+//
+
+import Foundation
+
+//Class for Bike object
+class Bike {
+    
+//    var owner: String
+//    var available: Bool
+//    var longitude: Double
+//    var latitude: Double
+    
+}
---
+++ Bear Bikes/Model/Map/MapPin.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  Bear Bikes
+//
+//  Created by Ian Shen on 4/28/20.
+//  Copyright © 2020 Bear Bikes Team. All rights reserved.
+//
+
+import Foundation
+
+class MapPin {
+    
+}
