2d4722def4938b024196b3d15f1acb0e9042b377
Ian Shen <ian891104@berkeley.edu>
2020-04-30 20:23:13 +0800

    removed Pods and updated .gitignore

--- Pods/Target Support Files/Pods-Bear Bikes-Bear BikesUITests/Pods-Bear Bikes-Bear BikesUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Bear_Bikes_Bear_BikesUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Bear_Bikes_Bear_BikesUITests
-@end
--- Pods/Target Support Files/Pods-Bear Bikes/Pods-Bear Bikes-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Bear_Bikes : NSObject
-@end
-@implementation PodsDummy_Pods_Bear_Bikes
-@end
